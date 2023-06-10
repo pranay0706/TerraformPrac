b8b1aae128075ebf2f9d20ed0a63660a57393ed9
pranay0706 <55979454+pranay0706@users.noreply.github.com>
2023-06-10 17:55:56 +0530

    Updated local.tf

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 resource "local_file" "devops" {
         filename = "/home/ec2-user/terraform-prac/devops-automated.txt"
-        content = "We are going to be best DevOps Engineers"
+        content = "I want to be the best DevOps Engineer"
 }
