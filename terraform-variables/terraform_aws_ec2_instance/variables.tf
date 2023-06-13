5e7d5ddb916963e23fee5780eb02d1c8daa34d08
pranay0706 <55979454+pranay0706@users.noreply.github.com>
2023-06-13 07:39:16 +0530

    Updated Terraform Variables

---
+++ terraform-variables/terraform_aws_ec2_instance/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0f5ee92e2d63afc18"
+}
+
+variable "no_of_instance" {
+  default = 1
+}
+
+variable "instance_name" {
+  default = "test_ec2_instance"
+}
